2691138cc5d684a9eec613b0bdd4bdd31b1cc2c1
jcarpenter-uam <jcarpenter@uaminc.com>
2025-10-21 09:25:14 -0500

    testing new translation model

--- zoom-translation-server/services/translation_service.py
+++ zoom-translation-server/services/translation_service.py
@@ -1,7 +1,7 @@
-from models.translation import QwenTranslationModel
+from models.translation import HuggingFaceTranslationModel, QwenTranslationModel
 
 # TODO:
 # For now, we are hard-coding the Qwen model.
 # In the future, this could be a factory function that chooses a model
 # based on configuration.
-TranslationService = QwenTranslationModel
+TranslationService = HuggingFaceTranslationModel

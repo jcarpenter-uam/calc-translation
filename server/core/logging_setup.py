d81be1ce2b9610388105473fa55b4fcb007f1341
jcarpenter-uam <jcarpenter@uaminc.com>
2025-11-10 16:39:16 -0600

    renamed wih comments to remind me

---
+++ server/core/logging_setup.py
@@ -0,0 +1,3 @@
+# File to handle logging uniformly
+# Verbose log files should be saved per session at ./logs/{integration}/{session}/{integration}_{session}.log
+# Console logs are determined by the LOG_LEVEL env variable with debug, info, error levels

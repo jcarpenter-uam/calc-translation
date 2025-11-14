ca56979af091e6e68fd77f2a9fcec41944f60852
jcarpenter-uam <jcarpenter@uaminc.com>
2025-11-14 13:00:35 -0600

    dont need auth here

--- server/api/viewing.py
+++ server/api/viewing.py
@@ -1,10 +1,8 @@
 # /ws/view/{integration}/{id} || Endpoint for the web/desktop to connect to per session
 
-from fastapi import APIRouter, Depends, Path, WebSocket
+from fastapi import APIRouter, Path, WebSocket
 from services.connection_manager import ConnectionManager
 from services.viewer import handle_viewer_session
-
-# from services.auth_service import validate_token
 
 
 def create_viewer_router(viewer_manager: ConnectionManager) -> APIRouter:
@@ -18,7 +16,6 @@ def create_viewer_router(viewer_manager: ConnectionManager) -> APIRouter:
         websocket: WebSocket,
         integration: str = Path(),
         session_id: str = Path(),
-        # auth_ok: bool = Depends(validate_token),
     ):
         """
         WebSocket endpoint for clients to connect and view

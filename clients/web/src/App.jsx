108e4c17d7b05f6d9f3c45489c7a7c3989eebbb4
jcarpenter-uam <jcarpenter@uaminc.com>
2025-11-21 11:41:37 -0600

    auto scroll to top

--- clients/web/src/App.jsx
+++ clients/web/src/App.jsx
@@ -4,11 +4,13 @@ import SessionPage from "./pages/session";
 import Support from "./pages/support";
 import Privacy from "./pages/privacy";
 import Terms from "./pages/terms";
+import ScrollToTop from "./util/scroll-to-top";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100 transition-colors">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<LandingPage />} />
 
